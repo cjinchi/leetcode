8abf1c7deeeece93293c07dea7dc8c39f1ea2006
cjinchi <cjinchi@qq.com>
2019-01-16 13:04:30 +0800

    update

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -1,48 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
 //1
-class Solution {
+class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        //index map from new to old
-        int[] map = new int[nums.length];
-        for(int i=0;i<map.length;i++){
-            map[i] = i;
-        }
-        //sort
-        for (int i = 0; i < nums.length - 1; i++) {
-            int min = i;
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] < nums[min]) {
-                    min = j;
-                }
-            }
-            if (min != i) {
-                int temp = nums[i];
-                nums[i] = nums[min];
-                nums[min] = temp;
-
-                temp = map[i];
-                map[i] = map[min];
-                map[min] = temp;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int temp = target - nums[i];
+            if(map.containsKey(temp)){
+                return new int[]{map.get(temp),i};
+            }else{
+                map.put(nums[i],i);
             }
         }
-
-        //find
-        int left = 0, right = nums.length - 1;
-        int sum = nums[left] + nums[right];
-        while (sum != target) {
-            if (sum > target) {
-                right--;
-            } else {
-                left++;
-            }
-            sum = nums[left] + nums[right];
-        }
-
-        //return
-        return new int[]{map[left], map[right]};
+        //never reach here
+        return null;
     }
 
     public static void main(String[] args) {
-        Solution t = new Solution();
+        TwoSum t = new TwoSum();
         int[] nums = new int[]{-10,-1,-18,-19};
         for (int i : t.twoSum(nums, -19)) {
             System.out.println(i);
