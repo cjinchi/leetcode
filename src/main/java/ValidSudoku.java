99fee0106732cf4bf92c22c5bc250dba297965bb
cjinchi <cjinchi@qq.com>
2019-02-02 21:19:30 +0800

    update

--- src/main/java/ValidSudoku.java
+++ src/main/java/ValidSudoku.java
@@ -3,36 +3,15 @@ import java.util.Set;
 
 public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
-        for(int i=0;i<9;i++){
-            Set<Character> set1 = new HashSet<>();
-            Set<Character> set2 = new HashSet<>();
-            for(int j=0;j<9;j++){
-                if (board[i][j]!='.'&&set1.contains(board[i][j])){
-                    return false;
-                }else{
-                    set1.add(board[i][j]);
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
                 }
-
-                if(board[j][i]!='.'&&set2.contains(board[j][i])){
+                String str = "[" + board[i][j] + "]";
+                if (!set.add(i + str) || !set.add(str + j) || !set.add((i / 3) + str + (j / 3))) {
                     return false;
-                }else{
-                    set2.add(board[j][i]);
-                }
-            }
-
-        }
-
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                Set<Character> set = new HashSet<>();
-                for(int k=i*3;k<i*3+3;k++){
-                    for(int l=j*3;l<j*3+3;l++){
-                        if (board[k][l]!='.'&&set.contains(board[k][l])){
-                            return false;
-                        }else{
-                            set.add(board[k][l]);
-                        }
-                    }
                 }
             }
         }
